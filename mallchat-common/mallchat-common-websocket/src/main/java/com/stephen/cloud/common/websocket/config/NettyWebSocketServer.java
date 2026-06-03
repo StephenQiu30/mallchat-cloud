be9273618e40d042ad118661192201dbe3c571b4
StephenQiu30 <89136224+StephenQiu30@users.noreply.github.com>
2026-06-03 08:16:06 +0800

    impl: 心跳参数校验逻辑
    - 校验 heartbeatReaderIdle 必须大于 0
    - 校验 heartbeatWriterIdle 必须大于 0
    - 校验 heartbeatWriterIdle 必须小于 heartbeatReaderIdle

--- mallchat-common/mallchat-common-websocket/src/main/java/com/stephen/cloud/common/websocket/config/NettyWebSocketServer.java
+++ mallchat-common/mallchat-common-websocket/src/main/java/com/stephen/cloud/common/websocket/config/NettyWebSocketServer.java
@@ -64,6 +64,20 @@ public class NettyWebSocketServer {
             webSocketProperties.setPort(39999);
         }
 
+        // 校验心跳参数是否合法
+        Long readerIdle = webSocketProperties.getHeartbeatReaderIdle();
+        Long writerIdle = webSocketProperties.getHeartbeatWriterIdle();
+        if (readerIdle == null || readerIdle <= 0) {
+            throw new IllegalArgumentException("heartbeatReaderIdle 必须大于 0，当前值: " + readerIdle);
+        }
+        if (writerIdle == null || writerIdle <= 0) {
+            throw new IllegalArgumentException("heartbeatWriterIdle 必须大于 0，当前值: " + writerIdle);
+        }
+        if (writerIdle >= readerIdle) {
+            throw new IllegalArgumentException(
+                    "heartbeatWriterIdle 必须小于 heartbeatReaderIdle，当前值: writerIdle=" + writerIdle + ", readerIdle=" + readerIdle);
+        }
+
         // 设置 ChannelManager 的 serverId (IP:Port)
         try {
             // 这里简单取本地 IP，也可以从 Spring Cloud 环境获取
